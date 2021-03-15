5f4d1191c443aad670139dd53c9658b25e3059c1
Nadir Suhan ILTER <49036494+COD3M4ST3R@users.noreply.github.com>
2021-03-15 22:46:02 +0300

    First version of prototype: VP 1.1 - COD3M4ST3R: Updated GET method

--- app/src/main/java/com/prototype/Express/MainActivity.java
+++ app/src/main/java/com/prototype/Express/MainActivity.java
@@ -1,12 +1,37 @@
 package com.prototype.Express;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.os.StrictMode;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -15,6 +40,9 @@ public class MainActivity extends AppCompatActivity
     ImageView QR_imageView;
     TextView textView;
     TextView textView2;
+    TextView textView3;
+    Button button;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +55,8 @@ public class MainActivity extends AppCompatActivity
         textView = (TextView) findViewById(R.id.textView);
         textView2 = (TextView) findViewById(R.id.textView2);
         TextView textView_ProfileName = findViewById(R.id.textView_ProfileName);
+        button = findViewById(R.id.button);
+        textView3 = findViewById(R.id.textView3);
 
 
         // GETS PROFILE NAME FROM LOGIN PAGE
@@ -53,7 +83,67 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+
+        // TODO POST-GET TEST
+        requestQueue = Volley.newRequestQueue(this);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                jsonParse("pizzajungle");
+            }
+        });
+
     }
+
+
+
+
+
+
+    public void jsonParse(final String key)
+    {
+        String url_address = "http://104.248.207.133:5000/restaurants";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url_address, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("data");
+
+                    for(int i = 0; i < jsonArray.length(); i++)
+                    {
+                        JSONObject restaurant = jsonArray.getJSONObject(i);
+                        String restaurant_name = restaurant.getString("name");
+                        if(key.equals(restaurant_name))
+                        {
+                            int id = restaurant.getInt("id");
+                            textView3.append(String.valueOf(id));
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+
+
+
+
 
     public void openProfilePage()
     {
--- app/src/main/java/com/prototype/Express/SplashActivity.java
+++ app/src/main/java/com/prototype/Express/SplashActivity.java
@@ -3,14 +3,7 @@ package com.prototype.Express;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -19,52 +12,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-
-
-        // TODO POST-GET TEST
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        String url_address = "http://104.248.207.133:5000/api/v1/project-info";
-
-        try
-        {
-            StringBuffer response = new StringBuffer();
-            URL url = new URL(url_address);
-            URLConnection connection = url.openConnection();
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-
-            /*
-            Gson gson = new Gson();
-            String message = "hello";
-            message = gson.toJson(message);
-            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
-            wr.write(message);
-            wr.flush();
-            */
-
-            InputStream inputStream = connection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            int data = inputStreamReader.read();
-            while (data != -1) {
-                char current = (char) data;
-                data = inputStreamReader.read();
-                response.append(current);
-            }
-            String responseJSON = response.toString();
-            Toast.makeText(this, responseJSON, Toast.LENGTH_SHORT).show();
-
-
-        } catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-
-
-
-
-
 
 
 
