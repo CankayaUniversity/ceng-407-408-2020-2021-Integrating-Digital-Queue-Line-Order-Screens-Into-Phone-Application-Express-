12657a0a9d68340b949701c52501f5be2c75c651
can ozal <cannozall@gmail.com>
2021-04-13 17:07:47 +0000

    establish socket io connection

--- express-api/server.js
+++ express-api/server.js
@@ -4,44 +4,48 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-const errorHandler = require('./middleware/error')
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // Load env vars
 dotenv.config({
-    path: './config/config.env'
+  path: './config/config.env',
 });
 
 // Connect to database
 connectDB();
 
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Express API",
-            version: "1.0.0",
-            description: "Documentation for front-end and mobile developers who will use the Express API",
-            contact: {
-                //name: "Can Özal",
-                url: "https://github.com/CankayaUniversity/ceng-407-408-2020-2021-Integrating-Digital-Queue-Line-Order-Screens-Into-Phone-Application-Express-/wiki",
-                email: "cannozall@gmail.com"
-            }
-        },
-        servers: [{
-            url: "http://104.248.207.133:5000",
-        }, ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API',
+      version: '1.0.0',
+      description:
+        'Documentation for front-end and mobile developers who will use the Express API',
+      contact: {
+        //name: "Can Özal",
+        url:
+          'https://github.com/CankayaUniversity/ceng-407-408-2020-2021-Integrating-Digital-Queue-Line-Order-Screens-Into-Phone-Application-Express-/wiki',
+        email: 'cannozall@gmail.com',
+      },
     },
-    apis: ["./controllers/*.js"],
+    servers: [
+      {
+        url: 'http://104.248.207.133:5000',
+      },
+    ],
+  },
+  apis: ['./controllers/*.js'],
 };
 
 const specs = swaggerJsDoc(options);
 
-const app = express();
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 // Route files
 const restaurants = require('./routes/restaurants');
@@ -51,31 +55,35 @@ const menuItems = require('./routes/menuItems');
 // Body parser
 app.use(express.json());
 
-
 // Enable CORS
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/v1/restaurants', restaurants);
 app.use('/api/v1/menus', menus);
-app.use('/api/v1/menuitems', menuItems)
-
-
+app.use('/api/v1/menuitems', menuItems);
 
 app.use('/api/v1/project-info', (req, res, next) => {
-    res.json({
-        project_name: "express",
-        project_members: ["can", "suhan", "talha"]
-    })
+  res.json({
+    project_name: 'express',
+    project_members: ['can', 'suhan', 'talha'],
+  });
 });
 
 app.use(errorHandler);
 
+server.listen(5000);
 
-const server = app.listen(5000, () => {
-    console.log(`Server running in 5000`)
+io.on('connection', (socket) => {
+  console.log('someone connected');
+  socket.on('pc-send', (data) => {
+    io.sockets.emit('phone-receive', data);
+  });
+
+  socket.on('phone-send', (data) => {
+    io.sockets.emit('pc-receive', data);
+  });
 });
