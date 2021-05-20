82076152a927b543aac18b1a376403f756cbf5c7
Nadir Suhan ILTER <49036494+COD3M4ST3R@users.noreply.github.com>
2021-05-21 02:17:45 +0300

    First Release

---
+++ app/src/main/java/orderprojectexpress/prototype/Express/Activity/SplashActivity.java
@@ -0,0 +1,50 @@
+package orderprojectexpress.prototype.Express.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.prototype.Express.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // SPLASH SCREEN
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    open_LoginActivity();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    private void open_LoginActivity()
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void open_MainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    // TODO
+    private void open_NotificationActivity()
+    {
+        Intent intent = new Intent(SplashActivity.this, NotificationActivity.class);
+        startActivity(intent);
+    }
+}
